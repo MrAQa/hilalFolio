fcd8cad0cd4145328c386b69e2b594504ccc4d9b
mraqkha <abdulqadeer@senarios.co>
2024-02-26 21:40:31 +0500

    AQ :dsd

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,34 +14,31 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontSize: {
         28: "28px",
         35: "35px",
-        32:"32px"
+        32: "32px",
       },
       screens: {
-        'xs': '350px',
-        
+        xs: "350px",
       },
       colors: {
-        'lightThemeDelete': '#CD0000',
-        'lightThemeSuccess': '#098C26',
-        'primaryPurple' :'#6F4F9F',
-        'lightThemeSecondary' :'#747474',
+        lightThemeDelete: "#CD0000",
+        lightThemeSuccess: "#098C26",
+        primaryPurple: "#6F4F9F",
+        lightThemeSecondary: "#747474",
       },
-      maxWidth: {
-        sm: '100%',
-        md: '768px',
-        lg: '1024px',
-        xl: '1168px',
-        '2xl': '1320px',
-      },
+      // maxWidth: {
+      //   sm: '100%',
+      //   md: '768px',
+      //   lg: '1024px',
+      //   xl: '1168px',
+      //   '2xl': '1320px',
+      // },
     },
   },
   plugins: [],
-}
+};
