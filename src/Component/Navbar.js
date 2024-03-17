6c5cfdb062d96d82190122e5211c505baf08d179
mraqkha <abdulqadeer@senarios.co>
2024-03-17 15:36:02 +0500

    AQ : add code

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -512,7 +512,7 @@ const NavBar = () => {
                         </Link>
 
                         <Link
-                          to=''
+                          onClick={handleSignOut}
                           className="block rounded-md px-3 py-2 text-base font-medium text-primaryDark hover:bg-gray-700 hover:text-white"
                         >
                           Sign out
