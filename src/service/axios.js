fa68082b3eaa9a4390107c62680122d26d4b7d6a
Mohsin_Senarios <mohsinali.senarios@gmail.com>
2024-04-19 17:24:23 +0500

    Mohsin_ new build

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // we need to pass the baseURL as an object
 const API = axios.create({
 //   baseURL: process.env.REACT_APP_API,
-  baseURL:'http://18.141.200.39:8000/api',
+  baseURL:'http://18.140.56.180:8000/api',
 });
 
 export default API;
