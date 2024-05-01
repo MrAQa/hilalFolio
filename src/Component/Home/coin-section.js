07d18728cbbf1c8239301b11f99c1d4e5d051f80
masadraza1122 <127093948+masadraza1122@users.noreply.github.com>
2024-05-01 16:41:28 +0500

    markdown in report page

--- src/Component/Home/coin-section.js
+++ src/Component/Home/coin-section.js
@@ -21,6 +21,16 @@ const CoinSecton = ({ searchQuery, isLogedin }) => {
   const percentageChange = ['All', '1h', '24h', '7d'];
   useEffect(() => {
     setIsLoading(true)
+    fetchData()
+
+    const interval = setInterval(fetchData, 20000);
+
+    // Cleanup function to clear interval on component unmount
+    return () => clearInterval(interval);
+
+  }, [selectedStatus, selectedRank, selectedPercentage, isLogedin])
+
+  const fetchData = () => {
     let number = null;
     if (selectedRank !== 'All') {
       number = parseInt(selectedRank.match(/\d+/)[0], 10);
@@ -28,11 +38,9 @@ const CoinSecton = ({ searchQuery, isLogedin }) => {
     GetCmcData(selectedStatus, number, selectedPercentage).then((result) => {
       setIsLoading(false)
       if (result.success) {
-        // console.log(result?.body?.cmcData)
+
         const sortedData = result?.body?.cmcData?.sort((a, b) => a.cmc_rank - b.cmc_rank);
-        // const updatedCoinsData = sortedData.map((coin, i) => {
-        //   return { ...coin, favorite: false }; // Set favorite to false initially for each object
-        // });
+
         setCoinsData(sortedData)
         if (result?.body?.cmcData?.length === 0) {
           setNoDataFlag(true)
@@ -44,9 +52,7 @@ const CoinSecton = ({ searchQuery, isLogedin }) => {
     }).catch((err) => {
       console.log(err)
     })
-  }, [selectedStatus, selectedRank, selectedPercentage, isLogedin])
-
-
+  }
 
   const headCells = [
 
