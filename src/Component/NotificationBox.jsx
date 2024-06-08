6723d84e26b1444ff3dab1810f35cd9e38061751
masadraza1122 <127093948+masadraza1122@users.noreply.github.com>
2024-06-09 01:58:29 +0500

    update

--- src/Component/NotificationBox.jsx
+++ src/Component/NotificationBox.jsx
@@ -1,20 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Popover, Transition, Tab } from '@headlessui/react';
-import { BellNotificationIcon } from '../assets/custom-icon';
+import { BellNotificationIcon, GreenDot } from '../assets/custom-icon';
+import { GetAllNotification, ReadNotification } from '../service/service';
+import { useNavigate } from 'react-router-dom';
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ');
 }
 
 const NotificationBox = () => {
+    const [NotifData, useNotifData] = useState([]);
+    useEffect(() => {
+        fetchData()
+        const interval = setInterval(fetchData, 20000);
+        return () => clearInterval(interval);
+
+        // eslint-disable-next-line
+    }, [])
+
+    const fetchData = () => {
+        GetAllNotification().then((result) => {
+            useNotifData(result?.body?.notifications)
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+    const navigate = useNavigate()
+    const NotifNaviagte = (id) => {
+        navigate('/odr')
+        const data = {
+            ids: [
+                id
+            ],
+            all: false
+        }
+        ReadNotification(data).then((result) => {
+            if (result.success) {
+                fetchData()
+            }
+
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
     return (
         <Popover className="relative">
             {({ open }) => (
                 <>
                     <Popover.Button>
                         <BellNotificationIcon />
+                        <GreenDot className="absolute top-0 right-0" />
                     </Popover.Button>
-                    
+
                     <Transition
                         show={open}
                         as={React.Fragment}
@@ -25,18 +62,19 @@ const NotificationBox = () => {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                     >
-                        <Popover.Panel static className="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-md z-10">
+                        <Popover.Panel static className="absolute right-0 mt-2 w-96 bg-white shadow-lg rounded-md z-10">
+                        {({ close }) => (
                             <Tab.Group>
-                                <Tab.List className="flex border-b">
+                                <Tab.List className="flex border-b border-primaryPurple text-primaryPurple font-semibold">
                                     <Tab as={React.Fragment}>
                                         {({ selected }) => (
                                             <button
                                                 className={classNames(
                                                     'flex-1 py-2 px-4 focus:outline-none',
-                                                    selected ? 'bg-gray-200' : 'hover:bg-gray-200'
+                                                    selected ? 'border-b border-primaryPurple' : ''
                                                 )}
                                             >
-                                                Type 1
+                                                System
                                             </button>
                                         )}
                                     </Tab>
@@ -45,23 +83,34 @@ const NotificationBox = () => {
                                             <button
                                                 className={classNames(
                                                     'flex-1 py-2 px-4 focus:outline-none',
-                                                    selected ? 'bg-gray-200' : 'hover:bg-gray-200'
+                                                    selected ? 'border-b border-primaryPurple' : ''
                                                 )}
                                             >
-                                                Type 2
+                                                Alerts
                                             </button>
                                         )}
                                     </Tab>
                                 </Tab.List>
-                                <Tab.Panels className="p-4">
+                                <Tab.Panels className="h-[400px] overflow-y-auto">
                                     <Tab.Panel>
-                                        <p>Notifications for Type 1</p>
+                                        <p className='text-center p-4'>No Result Found</p>
                                     </Tab.Panel>
                                     <Tab.Panel>
-                                        <p>Notifications for Type 2</p>
+                                        <div onClick={() => close()}>
+
+                                            {NotifData?.map((item, index) => (
+                                                <div
+                                                    onClick={() => NotifNaviagte(item?._id)}
+                                                    key={`item-${index}`} className={`flex items-center gap-2 border-y-[1px] p-4 cursor-pointer hover:bg-slate-50 `}>
+                                                    <img className='size-5 rounded-full' src={item?.data?.logo} alt="logo" />
+                                                    <div>{item.title}</div>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </Tab.Panel>
                                 </Tab.Panels>
                             </Tab.Group>
+                                )}
                         </Popover.Panel>
                     </Transition>
                 </>
