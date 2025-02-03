5ae56cc5f3801f7c1bfac4e86043608330643a05
masadraza1122 <masadraza110@gmail.com>
2025-02-03 17:16:50 +0500

    update

--- src/Component/ODR/ReportedCoins.jsx
+++ src/Component/ODR/ReportedCoins.jsx
@@ -124,7 +124,7 @@ const ReportedCoins = ({ CoinsData, isLoadingCoins,setShowAssets }) => {
                                                         <>
                                                             {
                                                                 CoinsData?.map((item, index) => (
-                                                                    item?.reportGenerated &&
+                                                                    item?.status==='completed' &&
                                                                     <div key={index + '-item'}
                                                                         onClick={() => handleItemClick(item)}
                                                                         className='flex justify-between items-center max-h-24 gap-1 border-[1px] border-lightThemeOutline shadow-custom rounded-xl p-6 col-3-items'>
--- src/Pages/ODR.jsx
+++ src/Pages/ODR.jsx
@@ -3,7 +3,7 @@ import NavBar from '../Component/Navbar';
 import Footer from '../Component/Footer,';
 import HomeODR from '../Component/ODR/HomeODR';
 import CryptoAssets from '../Component/ODR/CryptoAssets';
-import { GetCmcData } from '../service/service';
+import { GetAllReport, GetCmcData } from '../service/service';
 import { useLocation } from 'react-router-dom';
 import Payment from '../Component/ODR/Payment';
 import ReportedCoins from '../Component/ODR/ReportedCoins';
@@ -50,13 +50,17 @@ const ODR = () => {
           const selectedItem = sortedData.find((item)=> item.symbol=== state?.symbol);
           handleItemClick(selectedItem);
         }
-        const reportedCoins = sortedData.filter((item) => item.reportGenerated || item.reportStatus!==null)
+        // const reportedCoins = sortedData.filter((item) => item.reportGenerated || item.reportStatus!==null)
        
-        setReportedCoins(reportedCoins)
+        // setReportedCoins(reportedCoins)
         
       }
     }).catch((err) => {
       console.log(err)
+    })
+    GetAllReport().then((response)=>{
+      // console.log(response);
+      setReportedCoins(response?.body?.reports)
     })
   }, [selectedStatus, selectedRank, selectedPercentage ,refresh])
 
